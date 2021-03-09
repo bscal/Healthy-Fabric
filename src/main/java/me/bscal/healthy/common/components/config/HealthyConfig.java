82042746f9104794a4fb4bbf5e574b5788a2bc09
Jon Fus <jonfus123@hotmail.com>
2021-03-08 23:54:58 -0500

    Added OroArmor's oro-config library
    - Working on a config class

---
+++ src/main/java/me/bscal/healthy/common/components/config/HealthyConfig.java
@@ -0,0 +1,34 @@
+package me.bscal.healthy.common.components.config;
+
+import com.oroarmor.config.Config;
+import com.oroarmor.config.ConfigItem;
+import com.oroarmor.config.ConfigItemGroup;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.util.List;
+
+import static com.google.common.collect.ImmutableList.of;
+
+public class HealthyConfig extends Config
+{
+
+	public static final ConfigItemGroup ROOT = new RootGroup();
+	public static final List<ConfigItemGroup> CONFIGS = of(ROOT);
+	public static final File CONFIG_FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), "healthy_config.json");
+
+	public HealthyConfig()
+	{
+		super(CONFIGS, CONFIG_FILE, "healthy_config");
+	}
+
+	public static class RootGroup extends ConfigItemGroup
+	{
+		public static final ConfigItem<Boolean> HUNGER_OVERHAUL = new ConfigItem<Boolean>("hungerOverhaul", true, "Overhaul_hunger");
+
+		public RootGroup()
+		{
+			super(of(HUNGER_OVERHAUL), "root");
+		}
+	}
+}
